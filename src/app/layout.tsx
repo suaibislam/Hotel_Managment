d674e2d0c7ebd46f4d59596ee8c0e5943f252584
Suaib <suaibs589@gmail.com>
2024-06-16 08:56:07 +0600

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["400", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>
+        <main className="font-normal">
+          <Header/>
+          {children}
+          </main>
+      </body>
     </html>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-wrap items-center justify-between'>
+        Hello Header
+        suaibislam
+        </header>
+  )
+}
+
+export default Header
